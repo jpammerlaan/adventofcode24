c98117246ef44a268493cd2e000e6fe0eead551c
Job Ammerlaan <job.ammerlaan@nn.nl>
2024-12-04 12:21:20 +0100

    Make it shorter but less readable, that will help!

--- adventofcode24/04/04.py
+++ adventofcode24/04/04.py
@@ -20,29 +20,25 @@ field = read_input_file(day='04', output_type='list')
 
 def part_one(field):
     # good luck reading all this, even I don't understand what I'm doing!
-    l2r = [row for row in field]
-    r2l = [row[::-1] for row in l2r]
-    t2b = [''.join(row) for row in zip(*field)]
-    b2t = [''.join(row[::-1]) for row in zip(*field)]
+    rows = [row for row in field]
+    rows += [row[::-1] for row in rows]
+    rows += [row for row in zip(*field)]
+    rows += [row[::-1] for row in zip(*field)]
 
     # get all diagonal lines within the map
     # thank god the grids are square
     n = len(field)
-    ul2br = [''.join([field[j][j] for j in range(n)])]
-    bl2ur = [''.join([field[n-j-1][j] for j in range(n)])]
+    diag = [[field[j][j] for j in range(n)]] + [[field[n-j-1][j] for j in range(n)]]
     for i in range(1, n):
-        ul2br.append(''.join([field[j][j+i] for j in range(n-i)]))
-        ul2br.append(''.join([field[j+i][j] for j in range(n-i)]))
-        bl2ur.append(''.join([field[n-j-1][j+i] for j in range(n-i)]))
-        bl2ur.append(''.join([field[n-i-j-1][j] for j in range(n-i)]))
-    br2ul = [row[::-1] for row in ul2br]
-    ur2bl = [row[::-1] for row in bl2ur]
+        diag.append([field[j][j+i] for j in range(n-i)])
+        diag.append([field[j+i][j] for j in range(n-i)])
+        diag.append([field[n-j-1][j+i] for j in range(n-i)])
+        diag.append([field[n-i-j-1][j] for j in range(n-i)])
+    rows += diag + [row[::-1] for row in diag]
 
-    rows = l2r + r2l + t2b + b2t + ul2br + br2ul + bl2ur + ur2bl
-    return sum(map(lambda row: row.count('XMAS'), rows))
+    return sum(map(lambda row: ''.join(row).count('XMAS'), rows))
 
 def part_two(field):
-    # here we go, we need a different approach
     n = len(field)
     xmas = 0
     for i in range(1, n-1):
