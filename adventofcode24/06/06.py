443d2e2723dd9fd0f396a9426306cc12cb6f8325
Job Ammerlaan <job.ammerlaan@nn.nl>
2024-12-06 12:44:34 +0100

    Day 6, unnecessary optimization is unnecessary

--- adventofcode24/06/06.py
+++ adventofcode24/06/06.py
@@ -15,55 +15,54 @@ def read_input_file(day, output_type='string'):
     return input_content
 
 
-grid = read_input_file(day='06', output_type='list')
-directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # ^ > v <
-
 def parse_grid(grid):
-    objs = {}
+    objs = set()
     x_max, y_max = len(grid[0]), len(grid)
     for y, line in enumerate(grid):
         for x, point in enumerate(line):
             if point == '#':
-                objs[(x, y)] = 1
+                objs.add((x, y))
             elif point in '^>v<':  # I thought the starting orientation was random, lol
-                pos = (x, y)
+                x0, y0 = x, y
                 d = '^>v<'.index(point)
-    return x_max, y_max, objs, pos, d
+    return x_max, y_max, objs, x0, y0, d
 
 
-def walk(x_max, y_max, objs, pos, d, directions):
-    seen = defaultdict(list, {pos: [d]})
-    loop = False
-    while 0 <= pos[0] <= x_max and 0 <= pos[1] <= y_max:
-        new_pos = (pos[0] + directions[d][0], pos[1] + directions[d][1])
-        if new_pos in objs.keys():
-            d = (d + 1) % len(directions)
-        else:
-            pos = new_pos
-        if d in seen[pos]:
-            loop = True
-            break
-        seen[pos].append(d)
-    return seen, loop
+def walk(x_max, y_max, objs, x, y, d, directions):
+    seen = defaultdict(list, {(x, y): [d]})
+    while 0 <= x <= x_max and 0 <= y <= y_max:
+        dx, dy = directions[d]
+        if (x + dx, y + dy) in objs:
+            d = (d + 1) % len(directions)  # rotate right 90 degrees
+            continue
+        x, y = (x + dx, y + dy)
+        # Check for loops, if we've seen this square in this direction already we're done
+        if d in seen[(x, y)]:
+            return
+        seen[(x, y)].append(d)
+    return seen
 
 
 def part_one(grid, directions):
-    x_max, y_max, objs, pos, d = parse_grid(grid)
-    seen, _ = walk(x_max, y_max, objs, pos, d, directions)
+    x_max, y_max, objs, x, y, d = parse_grid(grid)
+    seen = walk(x_max, y_max, objs, x, y, d, directions)
     return len(set(seen.keys())) - 1
 
 
 def part_two(grid, directions):
-    x_max, y_max, objs, pos, d = parse_grid(grid)
-    seen, _ = walk(x_max, y_max, objs, pos, d, directions)
+    x_max, y_max, objs, x, y, d = parse_grid(grid)
+    seen = walk(x_max, y_max, objs, x, y, d, directions)
     n = 0
     for square in seen.keys():
-        objs[square] = 1
-        _, loop = walk(x_max, y_max, objs, pos, d, directions)
-        n += loop
-        del objs[square]
+        objs.add(square)
+        if walk(x_max, y_max, objs, x, y, d, directions) is None:
+            n += 1
+        objs.remove(square)
     return n
 
+
+grid = read_input_file(day='06', output_type='list')
+directions = [(0, -1), (1, 0), (0, 1), (-1, 0)]  # ^ > v <
 
 print(f'Part one: {part_one(grid, directions)}')
 print(f'Part two: {part_two(grid, directions)}')
